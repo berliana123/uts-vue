410a722bc30c857fd7999360670fe0f7d38c0849
berliana123 <60633776+berliana123@users.noreply.github.com>
2020-03-22 18:05:58 +0700

    uts-vue2

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,14 +1,13 @@
 !<template>
   <div>
     <b-navbar toggleable="lg" type="light" variant="light" fixed="top">
-      <b-navbar-brand href="#">NavBar</b-navbar-brand>
+      <b-navbar-brand href="#">MY WEB</b-navbar-brand>
 
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
-          <b-nav-item to="/">Link</b-nav-item>
-          <b-nav-item to="/about" disabled>Disabled</b-nav-item>
+          <b-nav-item to="/">HOME</b-nav-item>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
