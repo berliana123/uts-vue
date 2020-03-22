1c0ed85664b85f90ae432996577ec5cf13680f2b
berliana123 <60633776+berliana123@users.noreply.github.com>
2020-03-22 17:56:25 +0700

    uts-vue2

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,59 +1,49 @@
 <template>
   <div class="hello">
-    <div class="pimg1 vh-100">
-      <div class="ptext">
-        <span class="border">Welcome To My Page</span>
-      </div>
-    </div>
+    <Carousel></Carousel>
     <section class="section section-light">
       <h1>{{ msg }}</h1>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur quos illum in molestiae harum recusandae aliquam, iste aut facere dolore?</p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur quos
+        illum in molestiae harum recusandae aliquam, iste aut facere dolore?
+      </p>
     </section>
 
     <div class="pimg2">
       <div class="ptext">
-        <span class="border trans">Pemandangan Satu</span>
+        <span class="border trans">Image Two Text</span>
       </div>
     </div>
     <section class="section section-dark">
       <h1>{{ msg }}</h1>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur quos illum in molestiae harum recusandae aliquam, iste aut facere dolore?</p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur quos
+        illum in molestiae harum recusandae aliquam, iste aut facere dolore?
+      </p>
     </section>
-
-    <div class="pimg3">
-      <div class="ptext">
-        <span class="border trans">Pemandangan Dua</span>
-      </div>
-    </div>
-    <section class="section section-dark">
-      <h1>{{ msg }}</h1>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur quos illum in molestiae harum recusandae aliquam, iste aut facere dolore?</p>
-    </section>
+    <b-row class="justify-content-center">
+      <Content></Content>
+    </b-row>
   </div>
 </template>
 
 <script>
+import Carousel from './Carousel'
+import Content from './Content'
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  components: {
+    Carousel,
+    Content
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-body,
-html {
-  height: 100%;
-  margin: 0;
-  font-size: 16px;
-  font-family: "Lato", sans-serif;
-  font-weight: 400;
-  line-height: 1.8em;
-  color: #666;
-}
-
 .pimg1,
 .pimg2,
 .pimg3 {
@@ -71,17 +61,17 @@ html {
 }
 
 .pimg1 {
-  background-image: url("../assets/img/image4.jpg");
+  background-image: url('../assets/image1.jpg');
   min-height: 400px;
 }
 
 .pimg2 {
-  background-image: url("../assets/img/image5.jpg");
+  background-image: url('../assets/image2.jpg');
   min-height: 400px;
 }
 
 .pimg3 {
-  background-image: url("../assets/img/image6.jpg");
+  background-image: url('../assets/image3.jpg');
   min-height: 400px;
 }
 
@@ -89,17 +79,18 @@ html {
   text-align: center;
   padding: 50px 80px;
 }
-
 .section-light {
   background-color: #f4f4f4;
   color: #666;
 }
-
 .section-dark {
   background-color: #282e34;
   color: #ddd;
 }
-
+.section:nth-child(odd) {
+  background-color: #f4f4f4;
+  color: #666;
+}
 .ptext {
   position: absolute;
   top: 50%;
